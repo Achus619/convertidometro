21f71f4630fc394d3fe2ef24872048ce501f3c2a
Achus619 <achus619@gmail.com>
2023-03-18 17:08:12 -0300

    Conversor básico

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,47 @@
-<h1>holis</h1>
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { buscarCotizaciones } from "./api/divisas";
+  import type { Cotizaciones } from "./api/divisas";
+
+  let cotizaciones: Cotizaciones;
+  let valorIngresado: number;
+
+  onMount(async () => {
+    cotizaciones = await buscarCotizaciones();
+  });
+
+  function yenesAPesos(yenes: number) {
+    if (!cotizaciones) return 0;
+    const yenesEnDolares = yenes / cotizaciones.dolarAYen;
+
+    return yenesEnDolares * cotizaciones.dolarTarjeta;
+  }
+
+  $: valorConvertido = yenesAPesos(valorIngresado);
+</script>
+
+<main class="container">
+  <h1>Convertidometro</h1>
+
+  {#if !cotizaciones}
+    <button aria-busy="true">carganding</button>
+  {:else}
+    <p>Dolar tarjeta: ${cotizaciones.dolarTarjeta}</p>
+    <p>USD = ¥{cotizaciones.dolarAYen}</p>
+  {/if}
+
+  <form>
+    <label for="yen-input"
+      >Yen
+      <input
+        bind:value={valorIngresado}
+        type="number"
+        name="yen-input"
+        placeholder="Numerito va aquí"
+        disabled={!cotizaciones}
+      />
+    </label>
+  </form>
+
+  <p>{valorConvertido}</p>
+</main>
---
+++ src/api/divisas.ts
@@ -0,0 +1,36 @@
+export async function cotizacionDolar(): Promise<number> {
+  const NOMBRE_DOLAR_OFICIAL = "Oficial";
+  const URL = "https://www.dolarsi.com/api/api.php?type=dolar";
+
+  const response = await fetch(URL).then((r) => r.json());
+  const dolarOficial = response.find(
+    (coso) => coso.casa.nombre === NOMBRE_DOLAR_OFICIAL
+  );
+  return parseFloat(dolarOficial?.casa?.venta?.replace(",", ".")) || 0;
+}
+
+export async function cotizacionYenEnDolar(): Promise<number> {
+  const URL =
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/jpy.min.json";
+
+  const response = await fetch(URL).then((r) => r.json());
+
+  return response?.jpy || 0;
+}
+export type Cotizaciones = {
+  dolarAYen: number;
+  dolarTarjeta: number;
+};
+export async function buscarCotizaciones(): Promise<Cotizaciones> {
+  const [dolarAYen, dolarOficial] = await Promise.all([
+    cotizacionYenEnDolar(),
+    cotizacionDolar(),
+  ]);
+
+  return {
+    dolarAYen,
+    dolarTarjeta: dolarOficialATarjeta(dolarOficial),
+  };
+}
+
+const dolarOficialATarjeta = (oficial: number) => oficial * 1.75;
