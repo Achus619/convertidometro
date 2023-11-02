4447f2901c6c00967938fdb56c6543adbbdde7e0
Achus619 <achus619@gmail.com>
2023-11-03 08:39:52 +0900

    guardar cotizaciones localstorage

--- src/api/divisas.ts
+++ src/api/divisas.ts
@@ -16,23 +16,58 @@ export async function buscarCotizaciones(): Promise<Cotizaciones> {
 }
 
 async function cotizacionDolar(): Promise<number> {
+  const valorEnLS = localStorage.getItem("convertidometro_usd");
+
+  if (valorEnLS) {
+    buscarCotizacionUSDGuardarLS();
+    return Number(valorEnLS);
+  }
+
+  return buscarCotizacionUSDGuardarLS();
+}
+
+async function buscarCotizacionUSDGuardarLS() {
   const NOMBRE_DOLAR_OFICIAL = "Oficial";
   const URL = "https://www.dolarsi.com/api/api.php?type=dolar";
 
-  const response = await fetch(URL).then((r) => r.json());
-  const dolarOficial = response.find(
-    (coso) => coso.casa.nombre === NOMBRE_DOLAR_OFICIAL
-  );
-  return parseFloat(dolarOficial?.casa?.venta?.replace(",", ".")) || 0;
+  try {
+    const response = await fetch(URL).then((r) => r.json());
+    const dolarOficial = response.find(
+      (coso) => coso.casa.nombre === NOMBRE_DOLAR_OFICIAL
+    );
+    const cotizacion = parseFloat(dolarOficial?.casa?.venta?.replace(",", "."));
+    localStorage.setItem("convertidometro_usd", String(cotizacion));
+
+    return cotizacion;
+  } catch (error) {
+    console.error("Fallo al buscar cotizacion usd");
+  }
 }
 
 async function cotizacionYenEnDolar(): Promise<number> {
+  const valorEnLS = localStorage.getItem("convertidometro_yen");
+
+  if (valorEnLS) {
+    buscarCotizacionYenGuardarLS();
+    return Number(valorEnLS);
+  }
+
+  return buscarCotizacionYenGuardarLS();
+}
+
+async function buscarCotizacionYenGuardarLS() {
   const URL =
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/jpy.min.json";
 
-  const response = await fetch(URL).then((r) => r.json());
+  try {
+    const response = await fetch(URL).then((r) => r.json());
+    const cotizacion = response?.jpy;
+    localStorage.setItem("convertidometro_yen", cotizacion);
 
-  return response?.jpy || 0;
+    return cotizacion;
+  } catch (error) {
+    console.error("Fallo al buscar cotizacion yen");
+  }
 }
 
 const MULTIPLICADOR_TARJETA = 2;
